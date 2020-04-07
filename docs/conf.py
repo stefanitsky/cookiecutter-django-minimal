2102d07208bdd0bd607148c2e280c26b9d631f18
Alexandr Stefanitsky-Mozdor <stefanitsky.mozdor@gmail.com>
2020-04-07 23:39:22 +0300

    🔖 Bump version: 0.2.0 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Alexandr Stefanitsky-Mozdor'
 author = 'Alexandr Stefanitsky-Mozdor'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
