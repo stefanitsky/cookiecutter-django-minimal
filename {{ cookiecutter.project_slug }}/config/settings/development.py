316ddbc2a3d59ceeda6bd7188ba53ce20f081883
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-07 21:38:45 +0300

    :wrench: configured development env / debug toolbar added

--- {{ cookiecutter.project_slug }}/config/settings/development.py
+++ {{ cookiecutter.project_slug }}/config/settings/development.py
@@ -6,3 +6,26 @@ class Development(settings.Base):
 
     # http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
     INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + settings.Base.INSTALLED_APPS
+    # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+    INSTALLED_APPS += ["debug_toolbar"]
+
+    # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+    MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + settings.Base.MIDDLEWARE
+
+    # Debug toolbar
+    DEBUG_TOOLBAR_CONFIG = {
+        "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+        "SHOW_TEMPLATE_CONTEXT": True,
+    }
+    # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+    INTERNAL_IPS = ["127.0.0.1"]
+    if env.bool("USE_DOCKER", default=True) is True:
+        import socket
+
+        hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+        INTERNAL_IPS += [ip[:-1] + "1" for ip in ips]
+
+    # Mail
+    EMAIL_HOST = "localhost"
+    EMAIL_PORT = 1025
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
